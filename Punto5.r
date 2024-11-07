d36e2e68c70a0bd35c24caf709b48c8eac3ca38f
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2024-11-07 14:07:44 -0500

    cambios

--- Punto5.r
+++ Punto5.r
@@ -1,5 +1,17 @@
 library(markovchain)
 
+# tasas y probabilidades
+tasa_llegadas <- 11.23308122
+tasa_etiquetado <- 2.647508995
+tasa_validacion <- 4.082838743
+p_exito_validacion <- 0.693069307
+p_error_validacion <- 0.306930693
+tasa_calidad <- 10.97860996
+p_exito_calidad <- 0.811881188
+p_error_calidad <- 0.188118812
+tasa_empaquetado <- 0.361074392
+
+
 # Sx etiquetado
 estados_etiquetado<-c(0:4)
 
@@ -13,8 +25,90 @@ estados_calidad<-c(0:3)
 estados_empaquetado<-c(0:3)
 
 # Sh todas las combinaciones
-estados_conjuntos <- expand.grid(Sx = estados_etiquetado,
-                                 Sy = estados_validacion,
-                                 Sz = estados_calidad,
-                                 Sw = estados_empaquetado)
+estados = c()
+
+for(i in estados_etiquetado){
+  for(j in estados_validacion){
+    for(k in estados_calidad){
+      for(l in estados_empaquetado){
+        estados = c(estados,paste(i,j,k,l, sep = ","))
+      }
+      
+    }
+  }
+}
+
+matrizQ=matrix(0, nrow=length(estados), ncol=length(estados))
+
+dimnames(matrizQ) = list(estados, estados)
+
+for (fila in estados){
+  for (columna in estados){
+    
+    #Para conocer los valores de cada variable de estado, vamos a dividir cada estado en sus tres componentes, utilizando strsplit. Para mayor facilidad, convertimos los estados de cada una de las variables en un valor numérico.
+    
+    i = as.numeric(strsplit(fila, ",")[[1]][1])
+    j = as.numeric(strsplit(fila, ",")[[1]][2])
+    k = as.numeric(strsplit(fila, ",")[[1]][3])
+    l = as.numeric(strsplit(fila, ",")[[1]][4])
+    
+    #Una vez más, realizamos la división de cada estado en sus tres componentes.
+    
+    m = as.numeric(strsplit(columna, ",")[[1]][1])
+    n = as.numeric(strsplit(columna, ",")[[1]][2])
+    o = as.numeric(strsplit(columna, ",")[[1]][3])
+    p = as.numeric(strsplit(columna, ",")[[1]][4])
+    
+    #Definición de la matriz de tasas de transición, de acuerdo con la formulación general mostrada antes.
+    
+    #Llegadas a la primera zona (prensas presión)
+    if(m == i + 1 & n == j & o == k & p == l & i < 4){
+      matrizQ[fila,columna] = tasa_llegadas
+    }
+    
+    #Procesamiento máquina satinadora (zona 3)
+    if(m == i-1 & n == j + 1 & o == k & p == l & i > 0 & j < 3){
+      matrizQ[fila,columna] = min(i,4)*tasa_etiquetado
+    }
+    
+    #Procesamiento zona recubrimiento (zona 2)
+    if(m == i & n == j - 1 & o == k + 1 & p == l & j > 0 & k < 3){
+      matrizQ[fila,columna] = tasa_validacion*(1-p_exito_validacion)
+    }
+    
+    #Procesamiento prensa presión (zona 1)
+    if(m == i & n == j  & o == k - 1 & p == l + 1 & k > 0 & l < 15){
+      matrizQ[fila,columna] = tasa_calidad*(1-p_error_calidad)
+    }
+    
+    if(m == i & n == j  & o == k - 1 & p == l & k > 0 ){
+      matrizQ[fila,columna] = tasa_calidad*(p_error_calidad)
+    }
+    
+    
+    if (m == i & n == j & o == k & p == max(l - 12, 0)) {
+      matrizQ[fila, columna] = tasa_empaquetado
+    }
+    
+    
+  }
+}
+
+row_sums <- rowSums(matrizQ)
+row_sums
+for(fila in estados){
+  for(columna in estados){
+    if(fila==columna){
+      suma = -rowSums(matrizQ)[fila]
+      matrizQ[fila,columna] = suma
+      
+    }
+  }
+}
+
+
+# Calculate row sums of the generator matrix
+row_sums <- rowSums(matrizQ)
+
+
 
